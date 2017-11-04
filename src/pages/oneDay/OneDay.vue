3ed375b4a75064eba932222ea7b7cee1f0983497
mengyan-11 <1431311430@qq.com>
2017-11-04 17:04:57 +0800

    list-yzz

---
+++ src/pages/oneDay/OneDay.vue
@@ -0,0 +1,16 @@
+<template>
+	  	<oneDay-header />
+</template>
+
+<script>
+import HeaderComponent from "./oneDayList";
+export default {
+	components: {
+		"oneDay-header": HeaderComponent
+	}
+}
+</script>
+
+<style>
+	
+</style>
---
+++ src/pages/oneDay/OneDayList.vue
@@ -0,0 +1,418 @@
+<template>
+<div class="list-page">
+	<div>
+		<ul>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="//img1.qunarzz.com/p/tts2/1707/43/8250a273d1946502.jpg_110x110_532f0563.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">[北京出发]天安门＋故宫【赠送珍宝馆】＋八达岭长城＋鸟巢·水立方一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">208</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售953</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts1/1605/35/f267ae654a8511f7.jpg_110x110_33358754.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【休闲纯玩半日游】恭王府.含大戏楼+什刹海+黄包车半日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">90</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售189</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts0/1511/7e/f82b6124019d78f7.jpg_110x110_f99e78f5.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【配备无线耳麦】故宫+珍宝馆深度纯玩半日游（含导游+门票</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">75</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售400</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts1/1505/c9/4cdfc7d9932008.jpg_110x110_16d9c8ff.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">北京八达岭长城＋庆丰包子铺+恭王府+鸟巢.水立方纯玩一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-cashback">
+						<span class="list-product-cashback">赠券</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">198</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售345</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts3/1708/e0/9072ffc0ef322302.jpg_110x110_52fb9421.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【7-12点天天发】八达岭长城+往返直通车+门票，自由可选！</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">45</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售7340</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts0/1707/e3/d77d245a07ca4102.jpg_110x110_b3252ca4.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【爆款】北京八达岭长城+十三陵定陵含地宫鸟巢纯玩一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">118</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售3976</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts1/1611/34/ce613ef51240ad02.jpg_110x110_fb7e8f08.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【5大景点】广场+故宫+八达岭长城+鸟巢水立方，北京一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">148</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售3476</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts4/1511/bb/0e9192fe4e7fcef7.jpg_110x110_058b32ed.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【深度文化游】故宫+恭王府+颐和园+鸟巢水立方纯玩一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">208</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售254</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts6/1707/af/2361187e42829e02.jpg_110x110_c1663208.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【天天发】北京古北水镇门票日景夜景+往返直通车+导游服务</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">88</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售894</span>
+					</div>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="list-img-con">
+					<img src="http://img1.qunarzz.com/p/tts2/1706/b9/68688c37bace6c02.jpg_110x110_32e4422c.jpg" class="list-img">
+					<span class="list-bookingflag">
+						<span class="list-bookingtext">可订明日</span>
+					</span>
+				</div>
+				<div class="list-productinfo">
+					<h4 class="list-productname">【10点天天发】北京八达岭长城+鸟巢水立方纯玩一日游</h4>
+					<div class="list-taglist">
+						<span class="list-tagitemlight">北京出发</span>
+						<span class="list-tagitem">无自费</span>
+						<span class="list-tagitem">无购物</span>
+					</div>
+					<div class="list-cashback">
+						<span class="list-product-cashback">赠券</span>
+					</div><div class="list-priceinfo"><span class="list-qunarprice">￥</span><em class="list-price">79</em><span class="list-priceflag">起</span>
+					</div>
+					<div class="list-moreinfo">
+						<span class="list-text">已售2061</span>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div class="list-moreinfoCon">
+		<div class="list-pageCon"> 
+			<div class="list-pagination">
+				<a href="javascript:void(0);" class="list-paginationbtn">上一页</a><span class="list-page-num">1 / 15</span><a href="javascript:void(0);" class="list-paginationbtn list-paginationnext">下一页</a>
+			</div>
+			<div class="list-page-text">去哪儿门票</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style>
+	.list-page{
+		background:#f5f5f5;
+	}
+	.list-item{
+		display:flex;
+		position:relative;
+		background:#fff;
+	}
+	.list-img-con{
+		margin:.2rem;
+		position:relative;
+		
+	}
+	.list-img{
+		width: 1.6rem;
+   		height: 1.6rem;
+	}
+	.list-bookingflag{
+		position: absolute;
+	    left: -.04rem;
+	    top: -.04rem;
+	    height: .24rem;
+	    padding: .02rem;
+	    background: #00bcd4;
+	    color: #fff;
+	    line-height: .24rem;
+	}
+	.list-bookingflag::after{
+		content: "";
+	    position: absolute;
+	    top: 0;
+	    right: -.1rem;
+	    width: 0;
+	    height: 0;
+	    border-top: .14rem solid #00bcd4;
+	    border-bottom: .14rem solid #00bcd4;
+	    border-right: .1rem solid transparent;
+	}
+	.list-bookingtext{
+	    display: block;
+	    font-size: .24rem;
+	    -webkit-transform: scale(.83)
+	}
+	.list-productinfo{
+		padding: .2rem .2rem .2rem 0;
+	    height: 1.6rem;
+	    overflow: hidden;
+	    border-bottom:1px solid #c9cccd;
+	    flex:1;
+	}
+	.list-cashback{
+		position: absolute;
+	    right: .02rem;
+	    top: .85rem;
+	    color: #ff1200;
+	    font-size: 0;
+	    border:1px solid #ff1200;
+	    -webkit-transform: scale(.5);
+	}
+	.list-product-cashback{
+		display: inline-block;
+	    width: 1.0rem;
+	    height: .4rem;
+	    font-size: .36rem;
+	    line-height: .32rem;
+	    text-align: center;
+	    color: #ff1200;
+	}
+	.list-productname{
+		line-height: .36rem;
+		font-size: .3rem;
+		display: -webkit-box;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+	}
+	.list-taglist{
+		position: relative;
+	    top: .06rem;
+	    overflow: hidden;
+	    max-height: .58rem;
+	    margin-right: 1rem;
+	    padding-top: .06rem;
+	    line-height: .24rem;
+	    font-size: 0;
+	}
+	.list-tagitemlight{
+		display: inline;
+	    padding: 0 .1rem;
+	    font-size: .24rem;
+	    color: #00bcd4;
+	}
+	.list-tagitem{
+		display: inline;
+	    padding: 0 .1rem;
+	    font-size: .24rem;
+	    color: #616161;
+	    border-left:1px solid #c9cccd;
+
+	}
+	.list-priceinfo{
+	    position: absolute;
+	    right: .2rem;
+	    bottom: .48rem;
+	    height: .32rem;
+	    line-height: .32rem;
+	    color: #9e9e9e;
+	    text-align: right;
+	}
+	.list-qunarprice{
+	    font-size: .22rem;
+	    font-weight: bold;
+	    color: #ff8300;
+	}
+	.list-price{
+		font-size: .32rem;
+	    padding-left: .04rem;
+	    font-weight: bold;
+    	color: #ff8300;
+	}
+	.list-priceflag{
+		font-size: .24rem;
+	    padding-left: .02rem;
+	}
+	.list-moreinfo{
+	    position: absolute;
+	    bottom: .2rem;
+	    width: 100%;
+	    font-size: 0;
+	    color: #9e9e9e;
+	}
+	.list-text{
+		position: relative;
+		left:-0.1rem;
+	    padding: 0 .1rem;
+    	font-size: .22rem;
+
+	}
+	.list-moreinfoCon{
+	    padding: .2rem 0;
+	    color: #00afc7;
+	    line-height: .82rem;
+	    background:#f5f5f5;
+
+	}
+	.list-pagination{
+	    height: .7rem;
+    	line-height: .7rem;
+    	text-align:center
+	}
+	.list-paginationbtn{
+	    background: #bdbdbd;
+	    color: #fff;
+	    border: 0;
+	    display: inline-block;
+   		width: 1.4rem;
+   		line-height: .6rem;
+    	border-radius: .06rem;
+    	text-align:center
+	}
+	.list-page-num{
+		color: #212121;
+	    line-height: .6rem;
+	    padding: 0 15px;
+	}
+	.list-page-text{
+		padding-top: .2rem;
+	    line-height: .3rem;
+	    font-size: .3rem;
+	    text-align:center;
+	}
+	.list-paginationnext{
+		background:#fff;
+		color:#00afc7;
+		border:1px solid #00afc7;
+
+	}
+</style>
