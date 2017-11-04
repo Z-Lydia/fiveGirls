549ac7fa0182925e7b2dbe4a2038f1811db3daa5
Lydia <2655163185@qq.com>
2017-11-04 09:56:36 +0800

    正式header

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 
 Vue.use(Router)//Vue使用Vue-router
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
+    	path: '/',
+      	name: 'Home',
+      	component: Home
+    },{
+      	path: '/list',
+      	name: 'List',
+      	component: List
+  	}
   ]
 })
