0ebb618611b8ecbde5d20f700dd8021b20e929ee
Lydia <2655163185@qq.com>
2017-11-06 21:14:45 +0800

    Lydia-ajax-vuex

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,31 +1,59 @@
 <template>
   	<div>
 		<home-header />
-		<week-swiper />
-		<home-category/>
-		<home-location />
-		<home-scenery />
-		<week-list />  	
+		<home-swiper :swiperInfo="this.$store.state.swiperInfo" />
+		<home-IconSwiper :iconSwiperInfo="this.$store.state.iconSwiperInfo" />
+		<home-activity :activityInfo="this.$store.state.activityInfo" />
+		<home-hotsale :hotsaleInfo="this.$store.state.hotsaleInfo" />
+		<home-week :weekInfo="this.$store.state.weekInfo" />  	
 	</div>
   
 </template>
 
 <script>
-import HeaderComponent from "./Header";
-import SceneryComponent from "./Scenery";
-import SwiperComponent from "./Swiper";
-import CategoryComponent from "./Category";
-import LocationComponent from "./Location";
-import WeekListComponent from "./WeekList";
+import Header from "./components/Header";
+import Swiper from "./components/Swiper";
+import IconSwiper from "./components/IconSwiper";
+import Activity from "./components/Activity";
+import Hotsale from "./components/Hotsale";
+import WeekList from "./components/WeekList";
+import axios from 'axios';
 
 export default {
+
 	components: {
-		"home-header": HeaderComponent,
-		"home-scenery": SceneryComponent,
-		"week-swiper": SwiperComponent,
-		"home-category":CategoryComponent,
-		"home-location":LocationComponent,
-		"week-list": WeekListComponent
+		"home-header": Header,
+		"home-swiper": Swiper,
+		"home-IconSwiper": IconSwiper,
+		"home-activity":Activity,
+		"home-hotsale":Hotsale,
+		"home-week": WeekList
+	},
+
+	methods: {
+		getHomeData() {
+			axios.get('/static/index.json')
+				.then(this.handleGetDataSucc.bind(this))
+				.catch(this.handleGetDataErr.bind(this))
+		},
+
+		handleGetDataSucc( response ) {
+			if (response.status === 200) { 
+				const {data}  = response.data;
+				this.$store.commit( "changeData",data );
+			}
+		},
+
+		handleGetDataErr( err ) {
+			console.log(err)
+		}
+	},
+
+	mounted() {
+		if (!this.$store.state.swiperInfo.length) {
+			this.getHomeData();
+		}
+		
 	}
 }
 </script>
---
+++ src/pages/home/components/IconSwiper.vue
@@ -0,0 +1,80 @@
+<template>
+	<swiper :options="swiperOption" ref="mySwiper">
+		<swiper-slide>
+
+			<div class="category-container">
+				<div class="category-img-container" v-for="(item,index) in iconSwiperInfo" :key="item.id" v-if="index<8">
+					<img :src="item.imgUrl" />
+					<div class="keywords">{{item.title}}</div>
+				</div>
+			</div>
+		</swiper-slide>
+		<swiper-slide>
+			<div>
+				<div div class="category-container">
+					<div class="category-img-container" v-for="(item,index) in iconSwiperInfo" :key="item.id" v-if="index>7 && index<16">
+						<img :src="item.imgUrl" />
+						<div class="keywords">{{item.title}}</div>
+					</div>
+				</div>
+			</div>
+
+		</swiper-slide>
+		<div class="swiper-pagination" slot="pagination"></div>
+	</swiper>
+</template>
+
+<script>
+	import {
+		swiper,
+		swiperSlide
+	} from 'vue-awesome-swiper'
+
+	export default {
+		props:[ "iconSwiperInfo" ],
+
+		data() {
+				return {
+					swiperOption: {
+						direction: 'horizontal',
+						autoHeight: true,
+						pagination: '.swiper-pagination',
+						observeParents: true,
+					}
+				}
+		},
+			components: {
+				swiper,
+				swiperSlide
+			}
+	}
+</script>
+
+<style scoped="scoped">
+	.category-container {
+		height: 3.7rem;
+		width: 100%;
+		padding-top: .1rem;
+		display: flex;
+		flex-wrap: wrap;
+		text-align: center;
+	}
+	
+	.category-img-container {
+		width: 25%;
+		height: 1.3rem;
+	}
+	
+	.keywords {
+		padding-top: .2rem;
+	}
+	
+	.category-img-container img {
+		width: .66rem;
+		height: .66rem;
+	}
+	
+	.swiper-img {
+		width: 100%;
+	}
+</style>
